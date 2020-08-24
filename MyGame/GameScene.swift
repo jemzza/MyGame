35912ee7861a3a4390b863795514f5f6c2d05464
jemzza <allidemocracy@gmail.com>
2020-08-24 12:01:55 +0300

    added score label

--- MyGame/GameScene.swift
+++ MyGame/GameScene.swift
@@ -13,7 +13,8 @@ struct PhysicsCategory {
     
     static let Ball: UInt32 = 0x1 << 1
     static let Land: UInt32 = 0x1 << 2
-    static let Obstrucion: UInt32 = 0x1 << 1
+    static let Obstrucion: UInt32 = 0x1 << 3
+    static let Score: UInt32 = 0x1 << 4
     
 }
 
@@ -26,23 +27,29 @@ class GameScene: SKScene {
         land.name = "myLand"
         return land
     }()
-    
-//    var obstruction = SKSpriteNode(color: .red, size: CGSize(width: 100, height: 100))
-    
+        
     var obstruction = SKNode()
 
     
     var moveAndRemove = SKAction()
     var gameStarted = Bool()
+    var score = Int()
+    var scoreLabel = SKLabelNode()
     
     override func didMove(to view: SKView) {
+        
+        self.physicsWorld.contactDelegate = self
+        scoreLabel.position = CGPoint(x: 0, y: self.frame.height / 4)
+        scoreLabel.fontColor = .white
+        scoreLabel.fontSize = 100
+        scoreLabel.text = "\(score)"
+        self.addChild(scoreLabel)
         
         land.position = CGPoint(x: 0, y: -self.frame.height / 2 + land.frame.height / 2)
         land.physicsBody = SKPhysicsBody(rectangleOf: land.size)
         land.physicsBody?.categoryBitMask = PhysicsCategory.Land
         land.physicsBody?.collisionBitMask = PhysicsCategory.Ball
         land.physicsBody?.contactTestBitMask = PhysicsCategory.Ball
-//        land.physicsBody?.pinned = true
         land.physicsBody?.affectedByGravity = false
         land.physicsBody?.isDynamic = false
         self.addChild(land)
@@ -52,7 +59,7 @@ class GameScene: SKScene {
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.frame.height / 2)
         ball.physicsBody?.categoryBitMask = PhysicsCategory.Ball
         ball.physicsBody?.collisionBitMask = PhysicsCategory.Land | PhysicsCategory.Obstrucion
-        ball.physicsBody?.contactTestBitMask = PhysicsCategory.Land | PhysicsCategory.Obstrucion
+        ball.physicsBody?.contactTestBitMask = PhysicsCategory.Land | PhysicsCategory.Obstrucion | PhysicsCategory.Score
         ball.physicsBody?.affectedByGravity = false
         ball.physicsBody?.isDynamic = true
         ball.zPosition = 2
@@ -72,13 +79,13 @@ class GameScene: SKScene {
                 self.createObstruction()
             }
             
-            let delay = SKAction.wait(forDuration: 2.5)
+            let delay = SKAction.wait(forDuration: 1.5)
             let spawnDelay = SKAction.sequence([spawn, delay])
             let spawnRepeatForever = SKAction.repeatForever(spawnDelay)
             self.run(spawnRepeatForever)
             
             let distance = CGFloat(self.frame.width + obstruction.frame.width)
-            let moveObstructions = SKAction.moveBy(x: -distance - 60, y: 0, duration: TimeInterval(0.01 * distance))
+            let moveObstructions = SKAction.moveBy(x: -distance - 400, y: 0, duration: TimeInterval(0.003 * distance))
             let removeObstructions = SKAction.removeFromParent()
             moveAndRemove = SKAction.sequence([moveObstructions, removeObstructions])
             
@@ -93,9 +100,24 @@ class GameScene: SKScene {
     
     func createObstruction() {
         
+        let randomHeight = CGFloat.random(min: 50, max: 200)
+        let randomWidth = CGFloat.random(min: 50, max: 100)
+        
+        let scoreNode = SKSpriteNode()
+        scoreNode.size = CGSize(width: 1, height: self.frame.height)
+        scoreNode.position = CGPoint(x: self.frame.width / 2 + randomWidth, y: land.frame.height + randomHeight)
+        scoreNode.physicsBody = SKPhysicsBody(rectangleOf: scoreNode.size)
+        scoreNode.physicsBody?.affectedByGravity = false
+        scoreNode.physicsBody?.isDynamic = false
+        scoreNode.physicsBody?.categoryBitMask = PhysicsCategory.Score
+        scoreNode.physicsBody?.collisionBitMask = 0
+        scoreNode.physicsBody?.contactTestBitMask = PhysicsCategory.Ball
+        scoreNode.color = SKColor.blue
+        
         obstruction = SKNode()
         
-        let landObstruction = SKSpriteNode(color: .red, size: CGSize(width: 100, height: 100))
+        
+        let landObstruction = SKSpriteNode(color: .red, size: CGSize(width: randomWidth, height: randomHeight))
         
         landObstruction.position = CGPoint(x: self.frame.width / 2 + landObstruction.size.width / 2, y: landObstruction.frame.height / 2 - self.frame.height / 2 + land.frame.height)
         landObstruction.physicsBody = SKPhysicsBody(rectangleOf: landObstruction.size)
@@ -105,13 +127,31 @@ class GameScene: SKScene {
         landObstruction.physicsBody?.isDynamic = false
         landObstruction.physicsBody?.affectedByGravity = false
         
+        obstruction.addChild(scoreNode)
         obstruction.addChild(landObstruction)
         landObstruction.zPosition = 1
+        
+        
         obstruction.run(moveAndRemove)
         self.addChild(obstruction)
     }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+}
+
+extension GameScene: SKPhysicsContactDelegate {
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        let firstBody = contact.bodyA
+        let secondBody = contact.bodyB
+        
+        if firstBody.categoryBitMask == PhysicsCategory.Score && secondBody.categoryBitMask == PhysicsCategory.Ball || firstBody.categoryBitMask == PhysicsCategory.Ball && secondBody.categoryBitMask == PhysicsCategory.Score {
+            
+            score += 1
+            scoreLabel.text = "\(score)"
+        }
     }
 }
--- MyGame/Random.swift
+++ MyGame/Random.swift
@@ -9,13 +9,13 @@
 import Foundation
 import CoreGraphics
 
-public extension CGFloat{
+extension CGFloat{
     
     static func random() -> CGFloat {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     }
     
-    static func random(min : CGFloat, max : CGFloat) -> CGFloat {
+    static func random(min: CGFloat, max: CGFloat) -> CGFloat {
         return CGFloat.random() * (max - min) + min
     }
     
